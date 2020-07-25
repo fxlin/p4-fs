d20de6d18a933f74e02256d541ce0bbcb93c5412
Felix Lin <linxz02@gmail.com>
2020-07-25 14:08:57 -0400

    first commit

---
+++ P4A_check.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+#
+# sanity check script for Project 4A
+#	tarball name
+#	tarball contents
+#	student identification 
+#
+LIBRARY_URL="www.cs.ucla.edu/classes/cs111/Software"
+LAB="lab4a"
+README="README"
+MAKEFILE="Makefile"
+
+IMAGES="my_beaglebone connected copy build_run git"
+FORMATS="png jpg gif"
+
+let errors=0
+
+if [ -z "$1" ]
+then
+	echo usage: $0 your-student-id
+	exit 1
+else
+	student=$1
+fi
+
+# get copy of our grading/checking functions
+if [ -s functions.sh ]; then
+	source functions.sh
+else
+	wget $LIBRARY_URL/functions.sh 2> /dev/null
+	if [ $? -eq 0 ]; then
+		>&2 echo "Downloading functions.sh from $LIBRARY_URL"
+		source functions.sh
+	else
+		>&2 echo "FATAL: unable to pull test functions from $LIBRARY_URL"
+		exit -1
+	fi
+fi
+	
+TEMP=`pwd`/"CS111_test.$LOGNAME"
+if [ -d $TEMP ]
+then
+	echo Deleting old $TEMP
+	rm -rf $TEMP
+fi
+mkdir $TEMP
+unTar $LAB $student $TEMP
+cd $TEMP
+
+echo "... checking for README file"
+checkFiles $README
+let errors+=$?
+
+echo "... checking for submitter ID in $README"
+ID=`getIDs $README $student`
+let errors+=$?
+
+echo "... checking for submitter email in $README"
+EMAIL=`getEmail $README`
+let errors+=$?
+
+echo "... checking for submitter name in $README"
+NAME=`getName $README`
+let errors+=$?
+
+echo "... checking slip-day use in $README"
+SLIPDAYS=0
+slips=`grep "SLIPDAYS:" $README`
+if [ $? -eq 0 ]
+then
+	slips=`echo $slips | cut -d: -f2 | tr -d \[:space:\]`
+	if [ -n "$slips" ]
+	then
+		if [[ $slips == ?([0-9]) ]]
+		then
+			SLIPDAYS=$slips
+			echo "    $SLIPDAYS days"
+		else
+			echo "    INVALID SLIPDAYS: $slips"
+			let errors+=1
+		fi
+	else
+		echo "    EMPTY SLIPDAYS ENTRY"
+		let errors+=1
+	fi
+else
+	echo "    no SLIPDAYS: entry"
+fi
+
+echo "... checking for Makefile"
+checkFiles $MAKEFILE
+let errors+=$?
+
+echo "... checking for required images"
+for i in $IMAGES 
+do
+	found=""
+	for f in $FORMATS
+	do
+		if [ -s $i.$f ]; then
+			found="$i.$f"
+		fi
+	done
+	if [ -n "$found" ]; then
+		echo "    $found ... OK"
+	else
+		echo "    $i ... NOT FOUND ($FORMATS)"
+		let errors+=1
+	fi
+done
+
+echo
+if [ $SLIPDAYS -eq 0 ]
+then
+	echo "THIS SUBMISSION WILL USE NO SLIP-DAYS"
+else
+	echo "THIS SUBMISSION WILL USE $SLIPDAYS SLIP-DAYS"
+fi
+
+echo
+echo "THE ONLY STUDENTS WHO WILL RECEIVE CREDIT FOR THIS SUBMISSION ARE:"
+commas=`echo $ID | tr -c -d "," | wc -c`
+let submitters=commas+1
+let f=1
+while [ $f -le $submitters ]
+do
+	id=`echo $ID | cut -d, -f$f`
+	mail=`echo $EMAIL | cut -d, -f$f`
+	echo "    $id    $mail"
+	let f+=1
+done
+echo
+
+# delete temp files, report errors, and exit
+cleanup $$ $errors
---
+++ README.md
@@ -0,0 +1,23 @@
+# Lab4: Filesystem image forensics
+
+In this lab, we will inspect filesystem images. We will reason about the filesystem structures, extract useful information (programmatically), and verify their integrity. 
+
+## Objective
+
+* (primary) experience with filesystem data structures
+* (primary) reverse-engineering binary data 
+
+## Overview
+
+This las has two phases. Each phase has its own description and assignments. 
+
+* [Interpretation](interpretation.md): parse given ext2 images and dump their key data structures.
+
+* [Consistency](consistency.md): check given ext2 images and report any inconsistency 
+
+  
+
+**Credits**: Derived from UCLA CS111
+
+
+
